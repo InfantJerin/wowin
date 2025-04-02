576a4ee7c878f29fb2933a410584c89efb05eb6d
InfantJerin <infantjerin@gmail.com>
2025-04-02 23:46:51 +0530

    fix the deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ctl/',
+  base: '/wowin/', // Adjusted base path for your repo
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   }
-}); 
+});
